fc5f3497401830bbc6231b50973265b225422b46
Hassan-Asif-Memon <hm0078275@gmail.com>
2026-09-02 20:30:14 +0500

    work started

--- react/src/Components/Home.jsx
+++ react/src/Components/Home.jsx
@@ -1,6 +1,16 @@
+import logo from '../assets/RIWAJ DULHA CORNER.png';
 function Home(){
     return(<>
-    
+    <div className="header">
+        <div className="logo">
+        <img id='logo' src={logo} alt="Riwaj Dulha Corner" />
+        <div className="logo-title">
+        <h1>RIWAJ</h1>
+        <p>-DULHA CORNER-</p>
+        </div>
+        </div>
+
+    </div>
     </>)
 }
 export default Home;
