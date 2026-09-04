e8bdb6c7a275e093b4ce537954560a1a1fa30d55
Hassan-Asif-Memon <hm0078275@gmail.com>
2026-09-04 20:20:18 +0500

    working on it

--- react/src/Components/Home.jsx
+++ react/src/Components/Home.jsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import logo from '../assets/RIWAJ DULHA CORNER.png';
+import {FaSearch,FaUser,FaHeart,FaShoppingBag} from "react-icons/fa";
 function Home(){
     return(<>
     <div className="header">
@@ -11,7 +13,23 @@ function Home(){
         </div>
         <div className="search-box">
             <input type="text" name="search" id="search" placeholder='search for sherwani ' />
+            <FaSearch/>
         </div>
+        <div className="header-icons">
+            <div>
+            <FaUser/>
+            <p>Login / Register</p>
+            </div>
+            <div>
+                <FaHeart/>
+                <p>Wishlist</p>
+            </div>
+            <div>
+                <FaShoppingBag/>
+                <p>Cart (0)</p>
+            </div>
+        </div>
+        
 
     </div>
     </>)
