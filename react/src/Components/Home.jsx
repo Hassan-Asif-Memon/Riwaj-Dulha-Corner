7ea1e5bbec00a9173f4302edefc86c28e748cea4
Hassan-Asif-Memon <hm0078275@gmail.com>
2026-09-07 21:42:56 +0500

    carousel added

--- react/src/Components/Home.jsx
+++ react/src/Components/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 import logo from '../assets/RIWAJ DULHA CORNER.png';
 import {FaSearch,FaUser,FaHeart,FaShoppingBag} from "react-icons/fa";
 function Home(){
@@ -31,6 +32,7 @@ function Home(){
         </div>
         
         </div>
+
         <div className="nav">
             <a href="#">HOME</a>
             <a href="#">SHERWANI</a>
@@ -42,6 +44,103 @@ function Home(){
             <a href="#">CUSTOM STITCHING</a>
             <a href="#">CONTACT US</a>
         </div>
+
+
+    {/* <Carousel>
+      <Carousel.Item>
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel> */}
+
+    <Carousel>
+
+        <Carousel.Item>
+
+          <div className="slide">
+            {/* <img src={} alt="" /> */}
+            <div className="slide-content">
+
+              <p>ROYAL LOOK FOR YOUR</p>
+
+              <h1>SPECIAL DAY</h1>
+
+              <span>
+                EXCLUSIVE SHERWANI COLLECTION
+              </span>
+
+              <button>
+                SHOP SHERWANI
+              </button>
+
+            </div>
+          </div>
+
+        </Carousel.Item>
+
+
+        <Carousel.Item>
+
+          <div className="slide slide-two">
+            <div className="slide-content">
+
+              <p>DISCOVER OUR</p>
+
+              <h1>ROYAL STYLE</h1>
+
+              <span>
+                PREMIUM DULHA COLLECTION
+              </span>
+
+              <button>
+                EXPLORE COLLECTION
+              </button>
+
+            </div>
+          </div>
+
+        </Carousel.Item>
+
+
+        <Carousel.Item>
+
+          <div className="slide slide-three">
+            <div className="slide-content">
+
+              <p>CRAFTED FOR YOUR</p>
+
+              <h1>BIG DAY</h1>
+
+              <span>
+                PERFECT FIT & CUSTOM STITCHING
+              </span>
+
+              <button>
+                VIEW COLLECTION
+              </button>
+
+            </div>
+          </div>
+
+        </Carousel.Item>
+
+      </Carousel>
     </>)
 }
 export default Home;
